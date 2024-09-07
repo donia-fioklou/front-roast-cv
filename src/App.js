d7faf53a7e2e766e5ff8b28dcbbc2cc4a04c3ea8
Elom Laurent <laziagbenyo@gmail.com>
2024-09-07 14:53:31 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+// import "./App.css";
+
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <header className="no_underline">
+  <NavBar/>
+  </header>;
 
 }
 
