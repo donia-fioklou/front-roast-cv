773fe711a2f3e91bdf181ad74fe9a6a4bcf35dd6
Elom Laurent <laziagbenyo@gmail.com>
2024-09-07 14:58:47 +0000

    fix merging conflit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@
 import NavBar from "./components/NavBar";
 
 function App() {
-  return <main >
-  <header className="no_underline">
-  <NavBar/>
-  </header>;
-  {/* add the navaition here use the header and add a nav here*/ }
-
-
-  {/* the body of the landing page here */ }
-
-
-  {/* the footer here */ }
- </main>;
-
+  return (
+    <main>
+      <header className="no_underline">
+        <NavBar />
+      </header>
+      ;{/* add the navaition here use the header and add a nav here*/}
+      {/* the body of the landing page here */}
+      {/* the footer here */}
+    </main>
+  );
 }
 
 export default App;
